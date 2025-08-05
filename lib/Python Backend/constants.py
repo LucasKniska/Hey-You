9e46d0e0e0142c7511cbeece55ca95a36669281c
Lucas Kniska <lucaskniska1@gmail.com>
2025-08-04 20:16:36 -0400

    Added bucket matching algorithm

--- lib/Python Backend/constants.py
+++ lib/Python Backend/constants.py
@@ -1,3 +1,5 @@
+from models import bucket
+
 # --- Constants ---
 
 MINUTES_FOR_NEW_MATCH = 10 # The amount of time a new match will last if it is not acted upon
@@ -8,3 +10,10 @@ USERS = "Users"
 NEW_MATCHES = "Matches"
 REJECTED_MATCHES = "RejectedMatches"
 COMPLETED_MATCHES = "CompletedMatches"
+
+
+BUCKETS = [
+    bucket.Bucket("West_Virginia_University", 39.64795, -79.96970),
+    bucket.Bucket("Columbia_University", 40.80754, -73.96254),
+    bucket.Bucket("Stanford_University", 37.42755, -122.170169)
+]
--- lib/utils/constants/api_constants.dart
+++ lib/utils/constants/api_constants.dart
@@ -3,22 +3,21 @@ class APIConstants {
   static const String tAPIKey = '';
 
   /// Endpoints for api
-  static const String imperial = 'http://192.168.1.8:8000';
-  static const String lakeview = 'http://192.168.1.120:8000';
-  static const String base3 = 'http://10.0.2.2:8000';
+  static const String awsAPI = 'http://52.205.156.87';
+  static const String localDev = 'http://10.0.2.2:8000';
 
-  static const String acceptMatch = '$base3/accept-match';
-  static const String deleteMatch = '$base3/reject-match';
-  static const String updateLocation = '$base3/update-location';
-  static const String completeMatch = '$base3/complete-match';
-  static const String cancelCompleteMatch = '$base3/cancel-complete-match';
-  static const String getPreviousConnections = '$base3/get-previous-connections';
-  static const String updateUserData = '$base3/save-user-record';
-  static const String updateQuestionAnswers = '$base3/update-question-answers';
-  static const String updateUserMatchData = '$base3/update-user-match-data';
-  static const String updateSearchFilters = '$base3/update-search-filters';
-  static const String createNewUser = '$base3/create-new-user';
-  static const String updateUserField = '$base3/update-user-field';
+  static const String acceptMatch = '$awsAPI/accept-match';
+  static const String deleteMatch = '$awsAPI/reject-match';
+  static const String updateLocation = '$awsAPI/update-location';
+  static const String completeMatch = '$awsAPI/complete-match';
+  static const String cancelCompleteMatch = '$awsAPI/cancel-complete-match';
+  static const String getPreviousConnections = '$awsAPI/get-previous-connections';
+  static const String updateUserData = '$awsAPI/save-user-record';
+  static const String updateQuestionAnswers = '$awsAPI/update-question-answers';
+  static const String updateUserMatchData = '$awsAPI/update-user-match-data';
+  static const String updateSearchFilters = '$awsAPI/update-search-filters';
+  static const String createNewUser = '$awsAPI/create-new-user';
+  static const String updateUserField = '$awsAPI/update-user-field';
 }
 
 class Decisions {
